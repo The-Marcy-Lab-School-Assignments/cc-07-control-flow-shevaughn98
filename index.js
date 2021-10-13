16bc6e4ec46b30bb75611c9ed01cd0ec1a5b338e
shevaughn98 <shevaughnlegall@gmail.com>
2021-10-13 14:37:44 +0000

    challenge complete

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-//1
-function sumOfFourAndSix(){
+// //1
+// function sumOfFourAndSix(){
+//     let sum = 0
+//     for(let i = 1; i <= 1000; i++){
+//         if(i % 4 === 0 && i % 6 === 0){
+//             sum += i
+            
+//         } 
+//     } return sum; 
+    
+// }
+// console.log(sumOfFourAndSix());
+// //2
+// function oddAndEvenInFifteen(){
+//     for(let i = 0; i <= 15; i++){
+//         if(i % 3 === 0){
+//             console.log( i + "is odd"){
+    
+//         }else{
+//             if(i % 2 === 0){
+//                 console.log(i + " is even")
+//             }
+//         }
+//     }
   
-}
+// }
 
-//2
-function oddAndEvenInFifteen(){
-  
-}
-
+// //oddAndEvenInFifteen()
 //3
-function assignGrade(){
+function assignGrade(num){
+    if( num >= 91 ){
+        return "A"
+    }else{
+    if(num >= 81 ){
+        return "B"
+    }
+    }
+    
   
 }
 
@@ -17,4 +43,4 @@ function assignGrade(){
 
 
 
-
+console.log(assignGrade())
